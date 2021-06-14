164ffe378f75edd21b41f84ddc10a546cad0599d
Oscar Kemuel <oscar.kemuel5@gmail.com>
2021-06-14 14:18:12 -0300

    negatives5 completed

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -4,9 +4,17 @@ using std::cout;
 
 const int SIZE = 5; // input size.
 
-int main(void)
-{
+int main(void){
     // TODO: Adicione aqui seu código.
+    int num = 0;
+    int cont = 0;
+
+    for (int i = 0; i < SIZE; i++){
+        std::cin >> num;
+        if(num < 0) cont++;
+    }
+
+    std::cout << cont;
 
     return 0;
 }
