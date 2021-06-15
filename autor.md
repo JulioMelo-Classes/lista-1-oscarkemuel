016b3cfa53ae9d709d38bb6c5199903e2d58f7b4
Oscar Kemuel <oscar.kemuel5@gmail.com>
2021-06-15 20:20:12 -0300

    fibonacci completed

--- autor.md
+++ autor.md
@@ -11,7 +11,7 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 - [X] Negativos 5
 - [X] Soma Vizinhos
 - [X] Intervalos
-- [ ] Fibonacci
+- [X] Fibonacci
 - [ ] Minmax
 - [ ] Inverter
 - [ ] Filtragem
--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,17 @@
 #include "function.h"
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
-{
-    // TODO: adicione o seu código aqui.
+std::vector<unsigned int> fib_below_n( unsigned int n ){
+    std::vector<unsigned int> numbers;
+    int aux;
+    int last = 1, penultimate = 0;
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    for (int i = 0; (last + penultimate) < n; i++){
+      if(i==0) numbers.push_back(1);
+      numbers.push_back(last + penultimate);
+      aux = penultimate;
+      penultimate = last;
+      last += aux;
+    }
+
+    return numbers;
 }
